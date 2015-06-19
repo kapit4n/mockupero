8701f34edaafca09c1c84ab7035daa0298ac64c0
Luis Arce <luis.arce22@gmail.com>
2015-06-19 11:17:02 -0700

    Updated some variables to manage mockup list in project page

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -8,15 +8,67 @@
  * Controller of the myYoProjectApp
  */
 angular.module('myYoProjectApp')
-  .controller('ProjectCtrl', ['$scope', '$location', function ($scope, $location) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.addMockup = function () {
-      $location.path('/mockup-new');
-    };
-    $scope.workflow = {start: "start", close: "close", abandon: "abandon"};
-
-  }]);
+    .controller('ProjectCtrl', ['$scope', '$location', function($scope, $location) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+        $scope.addMockup = function() {
+            $location.path('/mockup-new');
+        };
+        $scope.workflows = [{
+            name: 'start',
+            functionName: '',
+            className : 'btn-primary'
+        }, {
+            name: 'close',
+            functionName: '',
+            className : 'btn-success'
+        }, {
+            name: 'abandon',
+            functionName: '',
+            className : 'btn-danger'
+        }];
+        $scope.mockups = [{
+            id: 0,
+            name: 'Mockup 1',
+            img: 'http://community.protoshare.com/wp-content/uploads/2010/12/example4-anim.gif',
+            description: 'abandon'
+        }, {
+            id: 1,
+            name: 'Mockup 2',
+            img: 'http://cameronbarrett.com/images/lg_ia1.gif',
+            description: 'abandon'
+        }, {
+            id: 2,
+            name: 'Mockup 3',
+            img: 'http://community.protoshare.com/wp-content/uploads/2010/12/example4-anim.gif',
+            description: 'abandon'
+        }, {
+            id: 3,
+            name: 'Mockup 4',
+            img: 'http://cameronbarrett.com/images/lg_ia1.gif',
+            description: 'abandon'
+        }, {
+            id: 0,
+            name: 'Mockup 1',
+            img: 'http://community.protoshare.com/wp-content/uploads/2010/12/example4-anim.gif',
+            description: 'abandon'
+        }, {
+            id: 1,
+            name: 'Mockup 2',
+            img: 'http://cameronbarrett.com/images/lg_ia1.gif',
+            description: 'abandon'
+        }, {
+            id: 2,
+            name: 'Mockup 3',
+            img: 'http://community.protoshare.com/wp-content/uploads/2010/12/example4-anim.gif',
+            description: 'abandon'
+        }, {
+            id: 3,
+            name: 'Mockup 4',
+            img: 'http://cameronbarrett.com/images/lg_ia1.gif',
+            description: 'abandon'
+        }];
+    }]);
