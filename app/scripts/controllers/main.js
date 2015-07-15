5884b31a7b0a67ef1aa942f42825a9bd6096bafc
Luis Arce <luis.arce22@gmail.com>
2015-07-14 21:12:32 -0400

    Updated pages to work with projects from database

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,24 @@
  * Controller of the myYoProjectApp
  */
 angular.module('myYoProjectApp')
-  .controller('MainCtrl', ['$scope', 'mockupService', '$location', '$rootScope', function ($scope, mockupService, $location, $rootScope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.addProject = function () {
-      $location.path('/project-new');
-    };
+    .controller('MainCtrl', ['$scope', 'mockupService', '$location', '$rootScope', function($scope, mockupService, $location, $rootScope) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+            $scope.addProject = function() {
+                $location.path('/project-new');
+            };
+            $scope.projects = [];
+            $rootScope.breadcrumb = mockupService.breadcrumb['home'];
 
-    $rootScope.breadcrumb = mockupService.breadcrumb['home'];
-  }]);
+            mockupService.project1.get({
+                    userId: 123
+                })
+                .$promise.then(function(projects) {
+                        $scope.projects = projects;
+                });
+
+
+    }]);
--- app/scripts/services/myservice.js
+++ app/scripts/services/myservice.js
@@ -8,7 +8,7 @@
  * Service in the myYoProjectApp.
  */
 angular.module('myYoProjectApp')
-    .service('mockupService', function() {
+    .service('mockupService', function($resource) {
         // AngularJS will instantiate a singleton by calling "new" on this function
         var fac = {};
 
@@ -65,6 +65,8 @@ angular.module('myYoProjectApp')
 
 
         fac.projects = ['John', 'James', 'Jake'];
+
+        fac.project1 = $resource('http://localhost:1337/projects', {userId:'@id'}, {get: {method:'GET', isArray:true}});
 
         return fac;
     });
