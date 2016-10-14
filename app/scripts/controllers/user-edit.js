c76ac29021e30f4abf6d288564be98f61fb52e10
Luis Arce <luis.arce22@gmail.com>
2016-10-13 23:36:19 -0400

    Updated edit-user to update password of a user

--- app/scripts/controllers/user-edit.js
+++ app/scripts/controllers/user-edit.js
@@ -13,7 +13,8 @@ angular.module('mockuperApp')
             loginService.reloadScope();
             headerService.updateHeader('users');
             $scope.user = null;
-
+            $scope.password = '';
+            $scope.password_confirm = '';
             userService.userById.get({
                     userId: $routeParams.userId
                 })
@@ -26,11 +27,32 @@ angular.module('mockuperApp')
                 });
 
             $scope.saveUser = function() {
-                userService.updateUser.save({
-                    id: $scope.user.id
-                }, $scope.user, function(result) {
-                    $window.location.href = '#/user/' + $scope.user.id;
-                });
+                if ($scope.password) {
+                    $scope.err = undefined;
+                    var erro_aux = '';
+                    if ($scope.password != $scope.password_confirm) {
+                        erro_aux = "Confirmation password doesn't match \n";
+                    }
+                    if ($scope.password.length < 4) {
+                        erro_aux += "Password should be at least 4 characters";
+                    }
+                    if (erro_aux) {
+                        $scope.err = erro_aux;
+                    } else {
+                        $scope.user.password = $scope.password;
+                    }
+                }
+                if (!$scope.err) {
+                    userService.updateUser.save({
+                        id: $scope.user.id
+                    }, $scope.user, function(result) {
+                        $scope.err = undefined;
+                        $window.location.href = '#/user/' + $scope.user.id;
+                    }, function(err) {
+                        console.error(err);
+                        $scope.err = err;
+                    });
+                }
             }
 
             $scope.cancel = function() {
