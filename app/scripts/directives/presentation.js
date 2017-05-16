53bd1d3913e2401e43e3396fd939d493ed687b67
larce <luis.arce22@gmail.com>
2017-05-15 21:52:17 -0400

    Added function to show the presentation panel in full screen

--- app/scripts/directives/presentation.js
+++ app/scripts/directives/presentation.js
@@ -13,11 +13,27 @@ angular.module('mockuperApp')
             restrict: 'E',
             scope: false,
             link: function postLink(scope, element, attrs) {
+                
                 scope.goPrev = function() {
                     $("#myCarousel").carousel("prev");
                 };
+
                 scope.goNext = function() {
                     $("#myCarousel").carousel("next");
+                };
+
+                /*
+                 * This function show the page in full screen, it just works for chrome
+                */
+                scope.fullScreen = function() {
+                    document.documentElement.webkitRequestFullscreen();
+                };
+
+                /*
+                 * This function exits from full screen, it just works for chrome
+                */
+                scope.exitFullScreen = function() {
+                    document.webkitExitFullscreen();
                 };
 
             }
