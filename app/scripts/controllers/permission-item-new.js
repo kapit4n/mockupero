37239ef16edba6ac3554d39ac2038ad2e9c35228
larce <luis.arce22@gmail.com>
2017-05-17 19:07:03 -0400

    Updated permission item new page to save a permission item

--- app/scripts/controllers/permission-item-new.js
+++ app/scripts/controllers/permission-item-new.js
@@ -8,10 +8,36 @@
  * Controller of the mockuperApp
  */
 angular.module('mockuperApp')
-  .controller('PermissionItemNewCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('PermissionItemNewCtrl', ['$scope', '$window', '$location', '$cookieStore',
+        'permissionItemService', 'breadcrumbService', 'headerService',
+        function($scope, $window, $location, $cookieStore, permissionItemService,
+            breadcrumbService, headerService) {
+
+            headerService.updateHeader('permission-item');
+            $scope.pItemName = "";
+            $scope.pItemModel = "";
+            $scope.pItemDescription = "btn-default";
+
+            try {
+                $rootScope.breadcrumb = breadcrumbService.updateBreadcrumb('permission-item-new', 'Home');
+            } catch (e) {}
+
+            $scope.save = function(pItemForm) {
+                if (pItemForm.$valid) {
+                    permissionItemService.createPermissionItem.save({
+                        name: $scope.pItemName,
+                        description: $scope.pItemModel,
+                        description: $scope.pItemDescription
+                    }, function(result) {
+                        $location.path("/permission-item/" + result.id);
+                    }, function(err) {
+                        $scope.err = err;
+                    });
+                }
+            };
+
+            $scope.cancel = function() {
+                $location.path('/permission-item-list');
+            }
+        }
+    ]);
