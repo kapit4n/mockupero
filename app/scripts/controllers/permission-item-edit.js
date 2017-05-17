e19e73217ffe64b2655948caf2762cf066b0e3cf
larce <luis.arce22@gmail.com>
2017-05-17 19:57:46 -0400

    Updated permission edit page to save changed values

--- app/scripts/controllers/permission-item-edit.js
+++ app/scripts/controllers/permission-item-edit.js
@@ -8,10 +8,43 @@
  * Controller of the mockuperApp
  */
 angular.module('mockuperApp')
-  .controller('PermissionItemEditCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('PermissionItemEditCtrl', ['$scope', '$routeParams', '$rootScope', '$location', '$cookieStore',
+        'permissionItemService',
+        'loginService', 'headerService', 'breadcrumbService', 'permissionService',
+        function($scope, $routeParams, $rootScope, $location, $cookieStore, permissionItemService, loginService,
+            headerService, breadcrumbService, permissionService) {
+            loginService.reloadScope();
+            headerService.updateHeader('permissionItem');
+            $scope.user = null;
+            $scope.editMode = true;
+            $scope.err = "";
+
+            $scope.permissionItem = null;
+            permissionItemService.permissionItemById.get({
+                    permissionItemId: $routeParams.permissionItemId
+                })
+                .$promise.then(function(result) {
+                    $scope.permissionItem = result;
+                    console.log(result);
+                    $rootScope.breadcrumb = breadcrumbService.updateBreadcrumb('permission-item', $scope.permissionItem);
+                }, function(err) {
+                    console.error(err);
+                });
+
+            $scope.save = function(permissionItemForm) {
+                if (permissionItemForm.$valid) {
+                    permissionItemService.updatePermissionItem.save({
+                        id: $scope.permissionItem.id
+                    }, $scope.permissionItem, function(result) {
+                        $location.path("/permission-item/" + result.id);
+                    }, function(err) {
+                        $scope.err = err;
+                    });
+                }
+            }
+
+            $scope.cancel = function() {
+                $location.path("/permission-item/" + $scope.permissionItem.id);
+            }
+        }
+    ]);
